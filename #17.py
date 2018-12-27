da93937c8099a2e56f4de2141d2b229013eddb56
Bob Wei <bobqywei@gmail.com>
2018-12-27 16:24:34 -0500

    Find longest path given file system

--- #17.py
+++ #17.py
@@ -2,10 +2,11 @@ def longest_path_at_level(level, lines):
 	longest = ""
 
 	for i in range(len(lines)):
-		line = lines[i]
-
-		if "\t" * (level+1) in line:
-			name = "/" + line[level+1:]
+		curr_dir = lines[i]
+		# At each level, only dirs at 1 level lower are considered
+		if "\t" * (level+1) in curr_dir:
+			name = "/" + curr_dir[level+1:]
+			# Finds longest subpath of lower directory
 			subpath_name = name + longest_path_at_level(level+1, lines[i+1:])
 
 			if len(subpath_name) > len(longest):
@@ -18,11 +19,12 @@ def longest_path_at_level(level, lines):
 
 def longest_path(s):
 	lines = s.split('\n')
-	print(lines)
+	for line in lines:
+		print(line)
 	return lines[0] + longest_path_at_level(0, lines[1:])
 
 
-dir_string = "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext" # "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+dir_string = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext" # "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"
 longest = longest_path(dir_string)
 print("Longest Path: " + longest)
 print("Length: " + str(len(longest)))
