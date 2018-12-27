27374b09bef08d5859853f7a5abd25b474552086
Bob Wei <bobqywei@gmail.com>
2018-12-27 16:47:03 -0500

    Find minimum number of classrooms needed

--- #21.cpp
+++ #21.cpp
@@ -5,24 +5,26 @@
 using namespace std;
 
 int roomsNeeded(vector<pair<int, int>> &times){
+	// Make list of all events and their corresponding times
 	vector<pair<int, int>> events;
-
 	for (pair<int, int> p : times) {
 		events.push_back(make_pair(p.first, 1)); // start event
 		events.push_back(make_pair(p.second, -1)); // end event
 	}
 
+	// sort the events in order based on time
 	sort(events.begin(), events.end());
 
 	int roomsUsed = 0;
 	int maxRooms = 0;
 
 	for (pair<int, int> p : events) {
+		// increments or decrements depending on whether event is start or end
 		roomsUsed += p.second;
 		maxRooms = max(roomsUsed, maxRooms);
 	}
 
-	return max;
+	return maxRooms;
 }
 
 int main() {
